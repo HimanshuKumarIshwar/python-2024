2f2d184f9e46eaeec79b9b958bbb2491ea755e8e
Himanshu Kumar <himanshuishwar399@gmail.com>
2024-02-12 00:50:12 +0530

    Data type and internal working of it's

---
+++ 01_basics/data_type.py
@@ -0,0 +1,34 @@
+# Number
+data = 123
+print(data)
+
+# String
+
+username = "Himanshu"
+print(len(username))
+print(username[0])
+print(username[1:3])
+
+# List
+mylist = [1,2,3,[2.3,"'Hima"],99]
+print(mylist[2])
+print(len(mylist))
+
+# tuple
+mytup = (12, 22, 33)
+print(len(mytup))
+print(mytup[2])
+
+
+#dist
+
+myDist = {"one":"Lemon", "two":12, "nine": 9}
+print(myDist['nine'])
+print(len(myDist))
+
+# Boolean
+
+ 
+
+
+
---
+++ 01_basics/datatypes.md
@@ -0,0 +1,6 @@
+# object types / Data types
+Number : 1234, 3.1234, 3+4j, 0b111, Decimal(), Fraction()
+
+String : 'spam', "Bob's", b'a\x01c', u'sp\xc3m
+
+List : [1,[2, 'three'], 4.5], list(range(10))
